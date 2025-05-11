7fac61d2028a10e3b0c00499f8d941ff8435ff01
Hygie Lichtenthaeler <hygie@iMac.lan>
2025-05-11 23:41:24 +0200

    mise en place + filter ok

---
+++ lvl 1/n_queen/n_queen.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   n_queen.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 18:30:49 by hlichten          #+#    #+#             */
+/*   Updated: 2025/05/11 21:26:51 by hlichten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+typedef enum e_bool
+{
+    FALSE,
+    TRUE
+}    t_bool;
+
+t_bool    backtrack(parameters)
+{
+    // 1. Cas de base : Solution trouvee
+    if (condition d'arret)
+    {
+        // Stocker ou afficher la solution
+        return TRUE; // Si on cherche une seule solution
+    }
+
+    // 2. Exploration des choix possibles
+    while (choix parmi les possibilites actuelles)
+    {
+        // 3. Tester la validite
+        if (is_valid(choix))
+        {
+            appliquer_modification(choix);
+
+            // 4. Appel recursif
+            if (backtrack(nouvel etat))
+            {
+                return TRUE; // Arreter si une solution est trouvee
+            }
+
+            // 5. Backtracking : annuler les changements
+            annuler_modification(choix);
+        }
+    }
+    return FALSE;
+}*/
